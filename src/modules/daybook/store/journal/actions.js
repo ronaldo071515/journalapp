29f075de45587cf627c15511a3fba27c20461799
Ronaldo Torres <rtorres66@misena.edu.co>
2023-02-03 15:34:06 -0500

    configuración básica de vuex

---
+++ src/modules/daybook/store/journal/actions.js
@@ -0,0 +1,15 @@
+
+// export const myAction = async ({ commit }) => {
+
+// }
+export const loadEntries = async (/* { commit } */) => {
+
+}
+
+export const upadteEntry = async (/* { commit } */) => {
+
+}
+
+export const createEntry = async (/* { commit } */) => {
+
+}
